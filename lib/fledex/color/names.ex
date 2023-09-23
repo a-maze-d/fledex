a75ccb4638e6955a0f90b494b3ed75d64e56d3d0
maze <maze@reik.se>
2023-09-23 08:00:00 +0200

    and some final cleanup including removing the TODO Now also all the credo warnings ar gone

--- lib/fledex/color/names.ex
+++ lib/fledex/color/names.ex
@@ -1,20 +1,16 @@
 defmodule Fledex.Color.Names do
   alias Fledex.Color.LoadUtils
 
-  # TODO: Create functions for the color data so we can get fast access
   colors = LoadUtils.load_color_file()
   for color <- colors do
-    # IO.puts("some Text #{color.name}")
     name = color.name
     def unquote(name)(), do: unquote(Macro.escape(color))
-    # end # |> tap(& IO.puts Code.format_string! Macro.to_string &1)
   end
 
   @colors colors
   @color_names Enum.map(@colors, fn %{name: name} = _colorinfo -> name end)
   def colors do
     @colors
-    # unquote(Macro.escape(colors))
   end
 
   def names do
