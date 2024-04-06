5969a9cf2da108f76e9baf27bc59b52e871ae932
maze <maze@reik.se>
2024-04-06 23:21:49 +0200

    adding typespec to the converted (from macro to) component function

--- lib/fledex.ex
+++ lib/fledex.ex
@@ -149,7 +149,7 @@ defmodule Fledex do
   It is up to each component to define their own set of mandatory and optional
   parameters.
   """
-  # @spec component(atom, module, keyword) :: Macro.t
+  @spec component(atom, module, keyword) :: Fledex.Animation.Manager.config_t()
   def component(name, module, opts) do
     Dsl.create_config(name, module, opts)
   end
